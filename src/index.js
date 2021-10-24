22205d602c6d04732bbf4af432c10c7bf2edf15d
wink-aajlak <aajlak.wink@gmail.com>
2021-10-24 11:42:17 +0530

    determining season

--- src/index.js
+++ src/index.js
@@ -21,7 +21,9 @@ class App extends React.Component {
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay 
+                lat = {this.state.lat}
+            />;
         }
 
         return <div>Loading..................</div>
