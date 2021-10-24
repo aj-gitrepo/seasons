b9167edbaf06968eab6cb29c8af45662631040a0
wink-aajlak <aajlak.wink@gmail.com>
2021-10-24 08:38:29 +0530

    setState

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat:null,
+        };
+
         window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
+            position => {
+                this.setState({lat: position.coords.latitude});
+            },
             err => console.log(err)
         );
+    }
+    render() {
+
         return (
-            <div>Latitude: </div>
+            <div>Latitude: {this.state.lat}</div>
         );
     }
 }
